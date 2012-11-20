64826438568b1227d40e23d73bce79d5493c80ee
Elliott Lui <elliott.lui@gmail.com>
2012-11-20 03:46:20 -0800

    Contract buttons

--- app/controllers/contract_controller.rb
+++ app/controllers/contract_controller.rb
@@ -1,4 +1,10 @@
 class ContractController < ApplicationController
   def form
+  	@categories = ["General","Emergency & Office Visit","Allergy & Immunizations", "Rehabilitation","Family Planning & Pregnancy","Professional Services", "Medical Services", "Prescription Drugs", "Medical Disorder", "Chemical Dependency", "Other Specifications"]
+  	@location = "San Diego"
+  	@plan_type = "PPO"
+
+  	@category = "Allergy & Immunizations"
+  	@category_specs = ["Allergy Testing","Allergy Injection Services", "Antibiotics Serum", "Occupational/Travel Immunizations","Other Immunizations", "Injections for Infertility", "All other Injects (Office-Based)", "All other Injections (Self-Injectable)"]
   end
 end
